cb680d93ae9ed80211e48d11f6e6eb7a045f0a4f
Sean Oughton <seanoughton@gmail.com>
2018-07-10 21:41:16 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-  skip_before_action :authenticate_user, only: :create
+  skip_before_action :authenticate_user#, only: :create
 
   def create
     byebug
